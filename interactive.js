df54238d9e9e67dfc8da1e8e2677a87af4b81006
lizuaan <150228653+lizuaan@users.noreply.github.com>
2023-11-15 10:40:20 +0300

    Add files via upload

--- interactive.js
+++ interactive.js
@@ -11,3 +11,22 @@ document.querySelectorAll('.nav-link').forEach(link => {
       link.setAttribute('aria-current', 'page')
     }
   })
+
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("nav-link");
+  window.onscroll = () => {
+    sections.forEach(sec =>{
+      let top = window.scrollY;
+      let offset = sec.offsetTop;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
+      if (top >= offset && top < offset + height){
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector(`[href*='#${id}']`).classList.add("active");
+        }  
+          )
+
+      };
+    })
+  }
